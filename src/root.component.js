449b632063eba43566859f11ab150ec7e07f351e
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-01 13:27:24 -0600

    Add Child to root component

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Child from "./child.component";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const [showChild, toggleShowChild] = useState(false);
+
+  return (
+    <section>
+      <p>{props.name} is mounted!</p> Look at the README for a thorough
+      explanation.
+      <div>
+        <button type="button" onClick={() => toggleShowChild(!showChild)}>
+          Render a child with more lazy styles
+        </button>
+      </div>
+      <div>{showChild && <Child />}</div>
+    </section>
+  );
 }
