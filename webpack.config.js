fa82e136adda33361679815e886d391b0d99d762
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-28 14:56:18 -0600

    Use webpack-merge to customize config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,45 @@
 const singleSpaDefaults = require("webpack-config-single-spa-react");
+const { mergeWithRules } = require("webpack-merge");
+
+const mergeRulesByTestMatch = mergeWithRules({
+  module: {
+    rules: {
+      test: "match",
+      include: "replace",
+      exclude: "replace",
+      use: {
+        loader: "match",
+        options: "replace",
+      },
+    },
+  },
+});
 
 module.exports = (webpackConfigEnv) => {
-  const config = singleSpaDefaults({
+  const defaultConfig = singleSpaDefaults({
     orgName: "example",
     projectName: "lazy-style",
     webpackConfigEnv,
   });
 
-  // TODO: find a better way to merge these configs
-  // webpack-merge used to be an option but its api is now so bad that you have to manually do all this anyway
-  // see https://github.com/survivejs/webpack-merge/issues/146#issuecomment-688937285
-  // and https://github.com/survivejs/webpack-merge/pull/150
-  // so for now these rules will be modified manually
-  const cssRule = config.module.rules[2];
-  delete cssRule.include;
-  cssRule.exclude = /\.lazy\.css$/i;
-
-  // Webpack style-loader lazyStyleTag
-  // https://webpack.js.org/loaders/style-loader/#lazystyletag
-  config.module.rules.push({
-    test: /\.lazy\.css$/i,
-    use: [
-      { loader: "style-loader", options: { injectType: "lazyStyleTag" } },
-      "css-loader",
-    ],
+  const config = mergeRulesByTestMatch(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          include: undefined,
+          exclude: /\.lazy\.css$/i,
+        },
+        {
+          test: /\.lazy\.css$/i,
+          use: [
+            { loader: "style-loader", options: { injectType: "lazyStyleTag" } },
+            "css-loader",
+          ],
+        },
+      ],
+    },
   });
 
   return config;
